0b3a4c3a44a5179da403fbbce25075aebcc1f586
jonahcoleman23 <jonahcoleman23@gmail.com>
2019-10-30 00:02:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,5 @@
 var choclateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 var cities = ["New York", "San Francisco"]
+console.log(cities)
+cities = ["Philadelphia", ...cities]
+console.log(cities)
